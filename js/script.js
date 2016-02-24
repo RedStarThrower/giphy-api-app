bd229bff924b904775e54867a18a416888395e97
Natalia Podosinnikova <Natalia@Natalias-MacBook-Air.local>
2016-02-24 15:45:09 -0600

    search working

--- js/script.js
+++ js/script.js
@@ -1,23 +1,53 @@
 console.log($)
 
-var apiKey = "dc6zaTOxFJmzC"
+var apiKey = "&api_key=dc6zaTOxFJmzC"
 
-var baseUrl = "http://api.giphy.com/v1/gifs/random?api_key="
+var baseUrl ="http://api.giphy.com/v1/gifs/search?q="
 
-var fullUrl = baseUrl + apiKey
+var container = document.querySelector("#container")
 
-var pinkyPromise = $.getJSON(fullUrl)
+// Input
+
+var inputEl = document.querySelector("input")
+
+var doRequest = function(searchQuery) {
+		var fullUrl = baseUrl + searchQuery + apiKey
+		var pinkyPromise = $.getJSON(fullUrl)
+		pinkyPromise.then(showData)
+}
+
+
+var userSearch = function(keyEvent) {
+	var inputEl = keyEvent.target
+	if (keyEvent.keyCode === 13) {
+		var searchQuery = inputEl.value
+		inputEl.value = ""
+		doRequest(searchQuery)
+	}
+
+}
+
+inputEl.addEventListener("keydown", userSearch)
+
+
+// Display Gifs
 
 var showData = function(jSonData) {
-	console.log(jSonData)
-	var imageObj = jSonData.data
-	var container = document.querySelector("#container")
-	container.innerHTML = objectToHTML(imageObj)
+	//console.log(jSonData)
+	var newHTMLString = ""
+	var imageArr = jSonData.data
+	for (var i = 0; i < imageArr.length; i++) {
+		var imageObj = imageArr[i]
+		newHTMLString += objectToHTML(imageObj)
+		container.innerHTML = newHTMLString
+
+	}	
 }
 
 var objectToHTML = function(imageObj) {
-	var imageHTML = '<img class="gif" src="' + imageObj.image_url + '">'
+	var theUrl = imageObj.images.original.url
+	var imageHTML = '<img class="gif" src="' + theUrl + '">' 
 	return imageHTML
 }
 
-pinkyPromise.then(showData) // sets up showData to run when data is ready
+ // sets up showData to run when data is ready
